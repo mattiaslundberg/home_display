1ea8fd2f03bbb0d80461d62c3d39e2d17c47551b
Mattias Lundberg <me@mlundberg.se>
2021-01-17 17:56:28 +0100

    Dynamic display

--- lib/home_display/display.ex
+++ lib/home_display/display.ex
@@ -5,33 +5,43 @@ defmodule HomeDisplay.Display do
     GenServer.start_link(__MODULE__, nil, opts)
   end
 
-  @impl true
+  @impl GenServer
   def init(_) do
     {:ok, pid} =
       Inky.start_link(:phat, :red, %{
         hal_mod: Application.get_env(:inky, :hal_module, Inky.RpiHAL)
       })
 
+    send(self(), :redraw)
+
+    {:ok, %{inky_pid: pid, count: 1}}
+  end
+
+  @impl GenServer
+  def handle_info(:redraw, %{inky_pid: pid, count: count} = state) do
+    count = rem(count, 2)
+
     Inky.set_pixels(pid, fn x, y, _width, _height, _current ->
-      # Delightful checkerboard
       x_odd = rem(x, 2) != 0
       y_odd = rem(y, 2) != 0
 
       case x_odd do
         true ->
           case y_odd do
-            true -> :black
+            true -> if count == 1, do: :black, else: :accent
             false -> :accent
           end
 
         false ->
           case y_odd do
-            true -> :accent
+            true -> if count != 1, do: :black, else: :accent
             false -> :white
           end
       end
     end)
 
-    {:ok, nil}
+    Process.send_after(self(), :redraw, 1000)
+
+    {:noreply, %{state | count: count + 1}}
   end
 end
