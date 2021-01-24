a7a3b0e0be4d21a9b995f69ea58a84eb6a08cb01
Mattias Lundberg <me@mlundberg.se>
2021-01-24 14:52:31 +0100

    Configure tzdata to download to writable location

--- config/target.exs
+++ config/target.exs
@@ -125,6 +125,8 @@ config :home_display, :viewport, %{
   ]
 }
 
+config :tzdata, :data_dir, "/tmp"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
