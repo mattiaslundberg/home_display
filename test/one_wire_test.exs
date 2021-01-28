753e1b5085c9ce9e95f4cc54b11983b21947b9a7
Mattias Lundberg <me@mlundberg.se>
2021-01-28 18:06:44 +0100

    Fix onewire display

--- test/one_wire_test.exs
+++ test/one_wire_test.exs
@@ -4,6 +4,6 @@ defmodule HomeDisplay.OneWireTests do
   test "parse_data" do
     raw_data = "ae 01 0e 04 7f ff 02 10 5e : crc=5e YES\nae 01 0e 04 7f ff 02 10 5e t=26875 "
 
-    assert 26.9 == OneWire.parse_data(raw_data)
+    assert 27 == OneWire.parse_data(raw_data)
   end
 end
