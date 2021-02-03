886f8c70f62a94f4dec93833b45857b42f80f98d
Mattias Lundberg <me@mlundberg.se>
2021-02-03 09:01:35 +0100

    Raise receive timeout

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,5 @@
 import Config
 config :home_display, :main_scene, HomeDisplay.Scene.MainMock
 config :home_display, :influx_module, HomeDisplay.Reporters.InfluxConnectionMock
+
+config :ex_unit, assert_receive_timeout: 200
